3f4af18f4b6ef97372f88e4f3a042fefd68a1ba3
jw0211.choi <jw0211.choi@NO-JW0211-CHO01.tn.corp.samsungelectronics.net>
2017-01-23 16:48:28 +0900

    .

--- Study/src/Solution.java
+++ Study/src/Solution.java
@@ -1,68 +1,75 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Solution {
 	
 	static int T;
-	static int C;
 	static int N;
-	static int[] E = new int[1000000];
-	static int result;
-	static long L1, L2;
-
+	static int M;
+	static double[] E = new double[100];
+	static int[] I = new int[11];
+	
+	static double sum;
+	static double muscle;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		T = Integer.parseInt(br.readLine());
-		for (int inx = 0; inx < T; inx++) {
-			C = Integer.parseInt(br.readLine()) * 10000000;
-			N = Integer.parseInt(br.readLine());
-			
-			if (N != 0) {
-				for (int jnx = 0; jnx < N; jnx++) {
-					E[jnx] = Integer.parseInt(br.readLine());
-				}
-	
-				L1 = 0;
-				L2 = 0;
-				result = 0;
-				process();
-				if (2==result)
-					System.out.println("#"+(inx+1) + " yes " + L1 + " " + L2);
-				else
-					System.out.println("#"+(inx+1) + " danger");
-			}
-			else {
-				System.out.println("#"+(inx+1) + " danger");
-			}
-		}
 
+		T = Integer.parseInt(br.readLine());
+		
+		for (int inx = 0; inx < T; inx++) {
+			String[] nm = br.readLine().split(" ");
+			N = Integer.parseInt(nm[0]);
+			M = Integer.parseInt(nm[1]);
+			
+			sum = 0;
+			String[] estr = br.readLine().split(" ");
+			for (int jnx = 0; jnx < N; jnx++) {
+				E[jnx] = Double.parseDouble(estr[jnx]);
+				sum += E[jnx];
+			}
+			
+			muscle = 0;
+
+			process();
+			System.out.print("#"+(inx+1) + " " + (long)muscle);
+			for (int jnx = 0; jnx < M; jnx++) {
+				System.out.print(" " + (I[jnx]+1));
+			}
+			System.out.println();
+		}
 	}
 
 	static void process() {
-		Arrays.sort(E, 0, N);
+		final double avg = (double)sum / (M+1);
 		
-		int begin = 0;
-		int end = N-1;
+		double tired = 0; // 누적 피로도
+		int i_idx = 0; // 주사맞은 횟수
 		
-		result = 1;
-		
-		int sum = 0;
-		
-		while (begin < end) {
-			sum = E[begin] + E[end];
-			if (sum < C)
-				begin++;
-			else if (sum > C)
-				end--;
-			else {
-				result = 2;
-				L1 = E[begin];
-				L2 = E[end];
+		double nextTired = 0;
 
-				break;
+		for (int inx = 0; inx < N; inx++) {
+			muscle += tired * E[inx];
+			nextTired = tired+E[inx];
+
+			if (nextTired >= avg) {
+				if ((avg-tired) <= (nextTired-avg)) {
+					I[i_idx++] = inx-1;
+
+					if (inx < N-1)
+						muscle -= tired * E[inx];
+
+					tired = E[inx];
+				} else {
+					I[i_idx++] = inx;
+					tired = 0;
+				}
 			}
+			else {
+				tired += E[inx];
+			}
+
 		}
 	}
+
 }
--- Study/src/test/PreTest17022.java
+++ Study/src/test/PreTest17022.java
@@ -4,60 +4,66 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
+//6 	 9 	 7 	 11 	 5 	 16 	 4 	 10
+
 public class PreTest17022 {
 	
 	static int T;
 	static int N;
 	static int M;
-	static int[] E = new int[100];
+	static double[] E = new double[100];
 	static int[] I = new int[11];
 	
-	static long sum;
-	static long muscle;
+	static double sum;
+	static double muscle;
 	
 	public static void main(String[] args) throws Exception {
-//		long start = System.currentTimeMillis();
+		long start = System.currentTimeMillis();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("D:\\input2.txt")));
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("D:\\input.txt")));
 
 		T = Integer.parseInt(br.readLine());
 		
 		for (int inx = 0; inx < T; inx++) {
 			String[] nm = br.readLine().split(" ");
-			N = Integer.parseInt(nm[0]); M = Integer.parseInt(nm[1]);
+			N = Integer.parseInt(nm[0]);
+			M = Integer.parseInt(nm[1]);
 			
 			sum = 0;
 			String[] estr = br.readLine().split(" ");
 			for (int jnx = 0; jnx < N; jnx++) {
-				E[jnx] = Integer.parseInt(estr[jnx]);
+				E[jnx] = Double.parseDouble(estr[jnx]);
 				sum += E[jnx];
 			}
 			
 			muscle = 0;
 
 			process();
-			System.out.print("#"+(inx+1) + " " + muscle);
+			System.out.print("#"+(inx+1) + " " + (long)muscle);
 			for (int jnx = 0; jnx < M; jnx++) {
 				System.out.print(" " + (I[jnx]+1));
 			}
 			System.out.println();
 		}
-
-//		long end = System.currentTimeMillis();
-//		System.out.println((end-start) + " ms");
+		
+		long end = System.currentTimeMillis();
+		System.out.println((end-start) + " ms");
 	}
 
 	static void process() {
 		final double avg = (double)sum / (M+1);
 		
-		int tired = 0; // 누적 피로도
+		double tired = 0; // 누적 피로도
 		int i_idx = 0; // 주사맞은 횟수
+		
+		double nextTired = 0;
 
 		for (int inx = 0; inx < N; inx++) {
 			muscle += tired * E[inx];
+			nextTired = tired+E[inx];
 
-			if (tired <= avg && tired+E[inx] >= avg) {
-				if (Math.abs(tired-avg) <= Math.abs(tired+E[inx]-avg)) {
+			if (nextTired >= avg) {
+				if ((avg-tired) <= (nextTired-avg)) {
 					I[i_idx++] = inx-1;
 
 					if (inx < N-1)
@@ -72,6 +78,7 @@ public class PreTest17022 {
 			else {
 				tired += E[inx];
 			}
+
 		}
 	}
 
