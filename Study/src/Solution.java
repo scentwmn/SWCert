ca34b933b9ee6e6a9a804f1bfe425484943cb52d
jw0211.choi <jw0211.choi@NO-JW0211-CHO01.tn.corp.samsungelectronics.net>
2017-02-01 10:39:27 +0900

    .

--- Study/src/Solution.java
+++ Study/src/Solution.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -11,14 +9,12 @@ public class Solution {
 	static int T;
 	static int N;
 	static int M;
-	static List<int[]> A;
+	static int[] A = new int[101];
 	static int[] I = new int[101];
-
-//	static int[] accSum = new int[101];
+	
+	static long[] accSum = new long[101];
 
 	public static void main(String[] args) throws Exception {
-//		long start = System.currentTimeMillis();
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
@@ -29,18 +25,18 @@ public class Solution {
 			M = Integer.parseInt(st.nextToken());
 			
 			StringTokenizer st2 = new StringTokenizer(br.readLine());
-			int sum = 0;
-			A = new LinkedList<int[]>();
-			for (int jnx = 0; jnx < N; jnx++) {
+			long sum = 0;
+			for (int jnx = 0; jnx < A.length; jnx++) {
+				A[jnx] = 0;
 				I[jnx] = 0;
+				accSum[jnx] = 0;
 			}
 
-			for (int jnx = 0; jnx < N; jnx++) {
-				int inp = Integer.parseInt(st2.nextToken());
-				A.add(new int[]{jnx, inp});
+			for (int jnx = 1; jnx <= N; jnx++) {
+				A[jnx] = Integer.parseInt(st2.nextToken());
 				I[jnx] = 0; // 주사맞았는지 여부(1=맞음,0=안맞음)
-				sum += inp;
-//				accSum[jnx] = sum;
+				sum += A[jnx];
+				accSum[jnx] = sum;
 			}
 			
 			double avg = (double)sum / (M+1);
@@ -48,65 +44,46 @@ public class Solution {
 			int muscleDay = 0;
 			int tired = 0; // 피로도
 			int injectCnt = 0;
+			for (int jnx = 1; jnx <= N; jnx++) {
 
-			while (injectCnt < M) {
-				int curVal = 0;
-				int prevVal = 0;
-				tired = 0;
+				if (injectCnt < M && (tired + A[jnx]) > avg) {
+					if (jnx > 1 && Math.abs(avg-tired) <= Math.abs(avg-tired-A[jnx])) {
+						I[jnx-1] = 1;
+						injectCnt++;
+						tired = 0;
 
-				for (int jnx = 0; jnx < A.size(); jnx++) {
-					prevVal = curVal;
-					curVal = A.get(jnx)[1];
-
-					if (jnx > 0 && tired >= avg) {
-						if (Math.abs(avg-tired) > Math.abs(tired+curVal-avg)) {
-							I[A.get(jnx)[0]-1] = 1;
-							injectCnt++;
-
-							sum -= curVal;
-						}
-						else {
-							I[A.get(jnx)[0]-2] = 1;
-							injectCnt++;
-
-							sum -= prevVal;
-						}
-						avg = (double)sum / (M-injectCnt+1);
-						break;
+						avg = (double)(sum-accSum[jnx-1]) / (M-injectCnt+1);
 					}
+					else {
+						I[jnx] = 1;
+						injectCnt++;
 
-					if (I[jnx] == 0) {
-						tired += curVal;
-					} else {
-						tired = curVal;
+						avg = (double)(sum-accSum[jnx]) / (M-injectCnt+1);
 					}
 				}
-			}
-
-			tired = 0;
-			muscleSum  = 0;
-			for (int jnx = 0; jnx < A.size(); jnx++) {
-				muscleSum += tired * A.get(jnx)[1];
 				
-				tired += A.get(jnx)[1];
-				if (I[jnx]==1)
-					tired = 0;
+				muscleDay = tired * A[jnx];
+				muscleSum += muscleDay;
+
+				// 오늘 주사를 맞았다면, 내일 피로도=0
+				tired = I[jnx] == 1 ? 0 : tired+A[jnx];
 			}
 
 			bw.write("#"+(inx+1)+ " " + muscleSum);
 			int outCnt = 0;
-			for (int jnx = 1; jnx <= 100 && outCnt < M; jnx++) {
+			for (int jnx = 1; jnx <= 100; jnx++) {
 				if (I[jnx] == 1) {
-					bw.write(" " + (jnx+1));
+					bw.write(" " + jnx);
 					outCnt++;
 				}
+				if (outCnt == M)
+					break;
 			}
 			bw.write('\n');
 			bw.flush();
 		}
 		
-//		long end = System.currentTimeMillis();
-//		System.out.println((end-start) + " ms");
-
+		bw.close();
+		br.close();
 	}
 }
